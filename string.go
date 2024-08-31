77be226f9226fe5e48497bcd2106c3443a3eb6a1
akbarnuri <makerctvvip@gmail.com>
2024-09-01 02:32:00 +0700

    learning golang

---
+++ string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Akbar")
+	fmt.Println("Nur")
+	fmt.Println("Ichfan")
+
+	fmt.Println(len("Akbar"))
+	fmt.Println("Nur"[0])
+	fmt.Println("Ichfan"[1])
+}
