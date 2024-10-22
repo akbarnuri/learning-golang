ca6fe852f089e6c8912d5cde3ff86b13bd45632f
akbarnuri <makerctvvip@gmail.com>
2024-10-23 02:15:48 +0700

    tipedataa go

--- tipedataa.go
+++ tipedataa.go
@@ -16,9 +16,24 @@ import "fmt"
 // 	fmt.Println(teks)
 // }
 
+// func main() {
+// 	var contohDecimal float32 = 1.66
+// 	fmt.Printf("%f\n", contohDecimal)
+// 	fmt.Printf("%.1f\n", contohDecimal)
+// 	fmt.Printf("%.3f\n", contohDecimal)
+// }
+
+// func main() {
+// 	var isMamal bool = true
+// }
+
+// package main
+
 func main() {
-	var contohDecimal float32 = 1.66
-	fmt.Printf("%f\n", contohDecimal)
-	fmt.Printf("%.1f\n", contohDecimal)
-	fmt.Printf("%.3f\n", contohDecimal)
+	var num1 int = 8
+	var num2 int = 10
+
+	var total int = num1 + num2
+
+	fmt.Printf("Total is :%d\n", total)
 }
