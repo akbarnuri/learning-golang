2023d185d2588854a4b2a13bc0f3d4900bfb879e
akbarnuri <makerctvvip@gmail.com>
2024-10-23 02:03:35 +0700

    tipedataa go

--- tipedataa.go
+++ tipedataa.go
@@ -7,11 +7,18 @@ import "fmt"
 // 	fmt.Println(teks)
 // }
 
+// func main() {
+// 	var teks string = `
+// 	Ini adalah "teks".
+// 	Dan ini dari 'baris baru'.
+// 	Okeee...
+// 	`
+// 	fmt.Println(teks)
+// }
+
 func main() {
-	var teks string = `
-	Ini adalah "teks".
-	Dan ini dari 'baris baru'.
-	Okeee...
-	`
-	fmt.Println(teks)
+	var contohDecimal float32 = 1.66
+	fmt.Printf("%f\n", contohDecimal)
+	fmt.Printf("%.1f\n", contohDecimal)
+	fmt.Printf("%.3f\n", contohDecimal)
 }
